bb409d6d82e370eb5bcb54c11b42cc9cf042bbfd
ikolpachkov <i.kolpachkov@gmail.com>
2016-01-14 15:00:28 +0300

    Initial Commit

---
+++ SlackController/SlackController.h
@@ -0,0 +1,19 @@
+//
+//  SlackController.h
+//  SlackController
+//
+//  Created by Igor Kolpachkov on 14.01.16.
+//  Copyright © 2016 ramotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlackController.
+FOUNDATION_EXPORT double SlackControllerVersionNumber;
+
+//! Project version string for SlackController.
+FOUNDATION_EXPORT const unsigned char SlackControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlackController/PublicHeader.h>
+
+
